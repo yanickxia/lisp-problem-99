0c30e06e7b68118822566a9fd88575b82fb7fa46
yann <me.yan.xia@qq.com>
2018-08-30 19:38:29 +0800

    Add ~p26

--- src/work_with_lists.clj
+++ src/work_with_lists.clj
@@ -188,6 +188,78 @@
 
 (assert (= (remove-at' '(a b c d) 2) '(a c d)))
 
+; P21 (*) Insert an element at a given position into a list.
+(defn insert-at [to-insert item at]
+  (concat (take (- at 1) item) (list to-insert) (drop (- at 1) item)))
+(assert (= (insert-at 'alfa '(a b c d) 2) '(a alfa b c d)))
+
+
+; P22 (*) Create a list containing all integers within a given range.
+(defn range' [start end]
+  (letfn [(range-with' [a start end]
+            (cond
+              (< start end) (range-with' (conj* a start) (inc start) end)
+              (= start end) (conj* a start)
+              (> start end) (range-with' (conj* a start) (dec start) end))
+            )]
+    (range-with' '() start end)))
+(assert (= (range' 4 9) '(4 5 6 7 8 9)))
+
+; P23 (**) Extract a given number of randomly selected elements from a list.
+(defn rnd-select [items n]
+  (let [n-at (rand-int (count items))]
+    (if (= 0 n)
+      '()
+      (concat (list (element-at items n-at))
+              (rnd-select (remove-at' items n-at) (dec n)))
+      )
+    ))
+
+(assert (= (count (rnd-select '(a b c d e f g h) 3)) 3))
+
+
+; P24 (*) Lotto: Draw N different random numbers from the set 1..M.
+(defn lotto-select [n maximum]
+  (rnd-select (range' 1 maximum) n))
+
+(assert (= (count (lotto-select 6 49)) 6))
+
+
+; P25 (*) Generate a random permutation of the elements of a list.
+(defn rnd-permu [item]
+  (rnd-select item (count item)))
+
+
+(defn flatten-1
+  "Flattens only the first level of a given sequence, e.g. [[1 2][3]] becomes
+   [1 2 3], but [[1 [2]] [3]] becomes [1 [2] 3]."
+  [seq]
+  (if (or (not (seqable? seq)) (nil? seq))
+    seq                                                     ; if seq is nil or not a sequence, don't do anything
+    (loop [acc [] [elt & others] seq]
+      (if (nil? elt) acc
+                     (recur
+                       (if (seqable? elt)
+                         (apply conj acc elt)               ; if elt is a sequence, add each element of elt
+                         (conj acc elt))                    ; if elt is not a sequence, add elt itself
+                       others)))))
+
+; P26 (**) Generate the combinations of K distinct objects chosen from the N elements of a list
+(defn combination [n item]
+  (letfn [(combination-with-latest [n item latest]
+            (if (= 0 n)
+              latest
+              (map (fn [x] (combination-with-latest (dec n) (remove #(= x %) item) (cons x latest))) item))
+            )]
+    (let [rs (combination-with-latest n item '())]
+      (letfn [(unwrap' [rs n]
+                (if (= 1 n)
+                  rs
+                  (unwrap' (flatten-1 rs) (dec n))))]
+        (unwrap' rs n)
+        ))
+    ))
+
 
 
 
